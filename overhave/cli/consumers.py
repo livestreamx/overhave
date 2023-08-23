a0afca4be1514850747ac15245da655a83b32d7e
m.shkut <m.shkut@tinkoff.ru>
2023-08-23 16:05:08 +0500

    consmer metrics

--- overhave/cli/consumers.py
+++ overhave/cli/consumers.py
@@ -3,13 +3,14 @@ import typer
 from overhave.base_settings import DataBaseSettings, LoggingSettings
 from overhave.cli.group import overhave
 from overhave.factory import ConsumerFactory
+from overhave.metrics import get_common_metric_container
 from overhave.transport import RedisStream
 
 
 def _run_consumer(stream: RedisStream) -> None:
     DataBaseSettings().setup_engine()
     LoggingSettings().setup_logging()
-    ConsumerFactory(stream=stream).runner.run()
+    ConsumerFactory(stream=stream, metric_container=get_common_metric_container()).runner.run()
 
 
 @overhave.command(short_help="Run Overhave Redis consumer")
