7477dff96409b5d440fc17ce20f5a7d5c99c5804
v.khadyev <v.khadyev@tinkoff.ru>
2023-12-21 18:36:30 +0500

    fix

--- tests/integration/admin/views/test_logout_view.py
+++ tests/integration/admin/views/test_logout_view.py
@@ -5,7 +5,7 @@ from flask.testing import FlaskClient
 
 from overhave import db, OverhaveAdminApp
 from overhave.storage import SystemUserModel
-from tests.db_utils import create_test_session
+from tests.db_utils import count_queries
 
 
 @pytest.mark.usefixtures("database")
@@ -13,15 +13,13 @@ class TestIndexView:
     """Tests for login view."""
 
     @pytest.mark.parametrize("test_user_role", [db.Role.user, db.Role.admin], indirect=True)
-    def test_logout_user_without_auto_redirect(
+    def test_logout_user_with_auto_redirect(
             self,
             test_app: OverhaveAdminApp,
             test_client: FlaskClient,
             test_authorized_user: SystemUserModel
     ) -> None:
-        with create_test_session():
-            response = test_client.get("/logout")
+        with count_queries(expected_count=2):
+            response = test_client.get("/logout", follow_redirects=True)
 
-        assert response.status_code == HTTPStatus.FOUND
-        assert ('You should be redirected automatically to the target URL: <a href="/login">/login</a>' in
-                response.data.decode("utf-8")), "Doesn't redirect to the index page"
+        assert response.status_code == HTTPStatus.OK
