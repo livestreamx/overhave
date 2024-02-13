a50b5689573a0e254a9f88e140e4b4ae24ab0ade
a.rogov <a.rogov@tinkoff.ru>
2024-02-13 12:49:04 +0500

    mda

--- overhave/transport/redis/deps.py
+++ overhave/transport/redis/deps.py
@@ -1,5 +1,6 @@
 import logging
 from functools import cache
+from typing import cast
 
 from redis import Redis
 from redis.sentinel import Sentinel
@@ -13,14 +14,17 @@ def make_sentinel_master(settings: OverhaveRedisSentinelSettings) -> Redis:
     logger.info("Connecting to redis through sentinel %s", settings.urls)
     url_tuples = [(url.host, url.port) for url in settings.urls if url.host is not None and url.port is not None]
     sentinel = Sentinel(url_tuples, socket_timeout=settings.socket_timeout.total_seconds(), retry_on_timeout=True)
-    return sentinel.master_for(settings.master_set, password=settings.password, db=settings.db)
+    return cast(Redis, sentinel.master_for(settings.master_set, password=settings.password, db=settings.db))
 
 
 def make_regular_redis(redis_settings: OverhaveRedisSettings) -> Redis:
-    return Redis.from_url(
-        redis_settings.url.human_repr(),
-        db=redis_settings.db,
-        socket_timeout=redis_settings.socket_timeout.total_seconds(),
+    return cast(
+        Redis,
+        Redis.from_url(
+            redis_settings.url.human_repr(),
+            db=redis_settings.db,
+            socket_timeout=redis_settings.socket_timeout.total_seconds(),
+        ),
     )
 
 
